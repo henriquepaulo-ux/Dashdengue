3333d42126b58ade8d8dde9eb2d9cf92a6ba01b9
Seu Nome <seuemail@exemplo.com>
2025-11-04 17:36:20 -0300

    corrigido para render

--- app.py
+++ app.py
@@ -181,5 +181,7 @@ def dashboard():
         tabela_html=tabela_html
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
